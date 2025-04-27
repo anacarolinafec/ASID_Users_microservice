e4eb0115aacb5cadc34d8dc92f95c27e0a2ffafb
anacarolinafec <137632049+anacarolinafec@users.noreply.github.com>
2025-04-27 16:04:43 +0100

    Primeiro commit user_service

---
+++ src/main/java/com/ijse/bookstore/entity/User.java
@@ -0,0 +1,32 @@
+package com.ijse.bookstore.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+
+    // to do the user microservice
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "userid")
+    private Long id;
+
+    @Column
+    private String fullname;
+
+    @Column(unique = true)
+    private String username;
+
+    @Column 
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+
+    private Long cart;
+
+
+}
